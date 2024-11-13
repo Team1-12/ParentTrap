fb8723c2726a135c3119882909eb137a795c5603
John Gibson <johnmgibson3@gmail.com>
2024-11-13 00:53:11 -0700

    Added dynamic db connection to the db.js file. Added first model, users.js, as an example. Essentially, db.js holds db connection info. That gets imported into model.js files. Those hold table objects (ex. users) with the CRUD functions. The model files get called info index.js where arrow functions pass form data to model objects.

---
+++ backend/models/users.js
@@ -0,0 +1,15 @@
+// This is an example of a model for a user table in the db. This allows us to create a Users object that has prewritten CRUD operations that can be called into index.js
+// backend/models/usersModel.js
+const db = require("../db/db");
+
+const Users = {
+    verifyUser: null , // this would be called when logging in to verify user
+    getUserById: (id) => db("users").where({ id }).first(),
+    
+    // When called, createUser pulls the information from the new user form (or any form that points to it) and inserts it into the users table.
+    // EX. Could also create an admin page that that lets you create or edit a user using this same createUser function
+    createUser: (userData) => db("users").insert(userData).returning("*"),
+    // Add other user-related queries here
+};
+
+module.exports = Users;
